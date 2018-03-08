bb4f4fe3fe9e3d20878e6bad3960cfa67268970d
Owner <Owner@BRAYDEN-SCHOENA>
2018-03-07 20:54:32 -0500

    Add project files.

---
+++ functionsPracticeNumber2/Form1.Designer.vb
@@ -0,0 +1,259 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmFunctionsPractice2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblAddOutput = New System.Windows.Forms.Label()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.txtAddNumber1 = New System.Windows.Forms.TextBox()
+        Me.txtAddNumber2 = New System.Windows.Forms.TextBox()
+        Me.btnResetAdd = New System.Windows.Forms.Button()
+        Me.btnResetSubtract = New System.Windows.Forms.Button()
+        Me.txtSubtractNumber2 = New System.Windows.Forms.TextBox()
+        Me.txtSubtractNumber1 = New System.Windows.Forms.TextBox()
+        Me.btnSubtract = New System.Windows.Forms.Button()
+        Me.lblDifferenceOutput = New System.Windows.Forms.Label()
+        Me.btnResetMultiplication = New System.Windows.Forms.Button()
+        Me.txtMultiplicationNumber2 = New System.Windows.Forms.TextBox()
+        Me.txtMultiplicationNumber1 = New System.Windows.Forms.TextBox()
+        Me.btnMulitply = New System.Windows.Forms.Button()
+        Me.lblProductOutput = New System.Windows.Forms.Label()
+        Me.btnResetDivision = New System.Windows.Forms.Button()
+        Me.txtDivisionNumber2 = New System.Windows.Forms.TextBox()
+        Me.txtDivisionNumber1 = New System.Windows.Forms.TextBox()
+        Me.btnDivide = New System.Windows.Forms.Button()
+        Me.lblQuotientOutput = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblAddOutput
+        '
+        Me.lblAddOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAddOutput.Location = New System.Drawing.Point(71, 128)
+        Me.lblAddOutput.Name = "lblAddOutput"
+        Me.lblAddOutput.Size = New System.Drawing.Size(100, 23)
+        Me.lblAddOutput.TabIndex = 3
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(71, 62)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(100, 23)
+        Me.btnAdd.TabIndex = 2
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'txtAddNumber1
+        '
+        Me.txtAddNumber1.Location = New System.Drawing.Point(6, 10)
+        Me.txtAddNumber1.Name = "txtAddNumber1"
+        Me.txtAddNumber1.Size = New System.Drawing.Size(100, 22)
+        Me.txtAddNumber1.TabIndex = 0
+        '
+        'txtAddNumber2
+        '
+        Me.txtAddNumber2.Location = New System.Drawing.Point(132, 10)
+        Me.txtAddNumber2.Name = "txtAddNumber2"
+        Me.txtAddNumber2.Size = New System.Drawing.Size(100, 22)
+        Me.txtAddNumber2.TabIndex = 1
+        '
+        'btnResetAdd
+        '
+        Me.btnResetAdd.Location = New System.Drawing.Point(71, 201)
+        Me.btnResetAdd.Name = "btnResetAdd"
+        Me.btnResetAdd.Size = New System.Drawing.Size(100, 23)
+        Me.btnResetAdd.TabIndex = 4
+        Me.btnResetAdd.Text = "Reset"
+        Me.btnResetAdd.UseVisualStyleBackColor = True
+        '
+        'btnResetSubtract
+        '
+        Me.btnResetSubtract.Location = New System.Drawing.Point(704, 203)
+        Me.btnResetSubtract.Name = "btnResetSubtract"
+        Me.btnResetSubtract.Size = New System.Drawing.Size(100, 23)
+        Me.btnResetSubtract.TabIndex = 9
+        Me.btnResetSubtract.Text = "Reset"
+        Me.btnResetSubtract.UseVisualStyleBackColor = True
+        '
+        'txtSubtractNumber2
+        '
+        Me.txtSubtractNumber2.Location = New System.Drawing.Point(765, 12)
+        Me.txtSubtractNumber2.Name = "txtSubtractNumber2"
+        Me.txtSubtractNumber2.Size = New System.Drawing.Size(100, 22)
+        Me.txtSubtractNumber2.TabIndex = 6
+        '
+        'txtSubtractNumber1
+        '
+        Me.txtSubtractNumber1.Location = New System.Drawing.Point(639, 12)
+        Me.txtSubtractNumber1.Name = "txtSubtractNumber1"
+        Me.txtSubtractNumber1.Size = New System.Drawing.Size(100, 22)
+        Me.txtSubtractNumber1.TabIndex = 5
+        '
+        'btnSubtract
+        '
+        Me.btnSubtract.Location = New System.Drawing.Point(704, 64)
+        Me.btnSubtract.Name = "btnSubtract"
+        Me.btnSubtract.Size = New System.Drawing.Size(100, 23)
+        Me.btnSubtract.TabIndex = 7
+        Me.btnSubtract.Text = "Subtract"
+        Me.btnSubtract.UseVisualStyleBackColor = True
+        '
+        'lblDifferenceOutput
+        '
+        Me.lblDifferenceOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblDifferenceOutput.Location = New System.Drawing.Point(704, 130)
+        Me.lblDifferenceOutput.Name = "lblDifferenceOutput"
+        Me.lblDifferenceOutput.Size = New System.Drawing.Size(100, 23)
+        Me.lblDifferenceOutput.TabIndex = 8
+        '
+        'btnResetMultiplication
+        '
+        Me.btnResetMultiplication.Location = New System.Drawing.Point(71, 506)
+        Me.btnResetMultiplication.Name = "btnResetMultiplication"
+        Me.btnResetMultiplication.Size = New System.Drawing.Size(100, 23)
+        Me.btnResetMultiplication.TabIndex = 14
+        Me.btnResetMultiplication.Text = "Reset"
+        Me.btnResetMultiplication.UseVisualStyleBackColor = True
+        '
+        'txtMultiplicationNumber2
+        '
+        Me.txtMultiplicationNumber2.Location = New System.Drawing.Point(132, 315)
+        Me.txtMultiplicationNumber2.Name = "txtMultiplicationNumber2"
+        Me.txtMultiplicationNumber2.Size = New System.Drawing.Size(100, 22)
+        Me.txtMultiplicationNumber2.TabIndex = 11
+        '
+        'txtMultiplicationNumber1
+        '
+        Me.txtMultiplicationNumber1.Location = New System.Drawing.Point(6, 315)
+        Me.txtMultiplicationNumber1.Name = "txtMultiplicationNumber1"
+        Me.txtMultiplicationNumber1.Size = New System.Drawing.Size(100, 22)
+        Me.txtMultiplicationNumber1.TabIndex = 10
+        '
+        'btnMulitply
+        '
+        Me.btnMulitply.Location = New System.Drawing.Point(71, 367)
+        Me.btnMulitply.Name = "btnMulitply"
+        Me.btnMulitply.Size = New System.Drawing.Size(100, 23)
+        Me.btnMulitply.TabIndex = 12
+        Me.btnMulitply.Text = "Multiply"
+        Me.btnMulitply.UseVisualStyleBackColor = True
+        '
+        'lblProductOutput
+        '
+        Me.lblProductOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblProductOutput.Location = New System.Drawing.Point(71, 433)
+        Me.lblProductOutput.Name = "lblProductOutput"
+        Me.lblProductOutput.Size = New System.Drawing.Size(100, 23)
+        Me.lblProductOutput.TabIndex = 13
+        '
+        'btnResetDivision
+        '
+        Me.btnResetDivision.Location = New System.Drawing.Point(704, 506)
+        Me.btnResetDivision.Name = "btnResetDivision"
+        Me.btnResetDivision.Size = New System.Drawing.Size(100, 23)
+        Me.btnResetDivision.TabIndex = 19
+        Me.btnResetDivision.Text = "Reset"
+        Me.btnResetDivision.UseVisualStyleBackColor = True
+        '
+        'txtDivisionNumber2
+        '
+        Me.txtDivisionNumber2.Location = New System.Drawing.Point(765, 315)
+        Me.txtDivisionNumber2.Name = "txtDivisionNumber2"
+        Me.txtDivisionNumber2.Size = New System.Drawing.Size(100, 22)
+        Me.txtDivisionNumber2.TabIndex = 16
+        '
+        'txtDivisionNumber1
+        '
+        Me.txtDivisionNumber1.Location = New System.Drawing.Point(639, 315)
+        Me.txtDivisionNumber1.Name = "txtDivisionNumber1"
+        Me.txtDivisionNumber1.Size = New System.Drawing.Size(100, 22)
+        Me.txtDivisionNumber1.TabIndex = 15
+        '
+        'btnDivide
+        '
+        Me.btnDivide.Location = New System.Drawing.Point(704, 367)
+        Me.btnDivide.Name = "btnDivide"
+        Me.btnDivide.Size = New System.Drawing.Size(100, 23)
+        Me.btnDivide.TabIndex = 17
+        Me.btnDivide.Text = "Divide"
+        Me.btnDivide.UseVisualStyleBackColor = True
+        '
+        'lblQuotientOutput
+        '
+        Me.lblQuotientOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblQuotientOutput.Location = New System.Drawing.Point(704, 433)
+        Me.lblQuotientOutput.Name = "lblQuotientOutput"
+        Me.lblQuotientOutput.Size = New System.Drawing.Size(100, 23)
+        Me.lblQuotientOutput.TabIndex = 18
+        '
+        'frmFunctionsPractice2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(877, 568)
+        Me.Controls.Add(Me.btnResetDivision)
+        Me.Controls.Add(Me.txtDivisionNumber2)
+        Me.Controls.Add(Me.txtDivisionNumber1)
+        Me.Controls.Add(Me.btnDivide)
+        Me.Controls.Add(Me.lblQuotientOutput)
+        Me.Controls.Add(Me.btnResetMultiplication)
+        Me.Controls.Add(Me.txtMultiplicationNumber2)
+        Me.Controls.Add(Me.txtMultiplicationNumber1)
+        Me.Controls.Add(Me.btnMulitply)
+        Me.Controls.Add(Me.lblProductOutput)
+        Me.Controls.Add(Me.btnResetSubtract)
+        Me.Controls.Add(Me.txtSubtractNumber2)
+        Me.Controls.Add(Me.txtSubtractNumber1)
+        Me.Controls.Add(Me.btnSubtract)
+        Me.Controls.Add(Me.lblDifferenceOutput)
+        Me.Controls.Add(Me.btnResetAdd)
+        Me.Controls.Add(Me.txtAddNumber2)
+        Me.Controls.Add(Me.txtAddNumber1)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.lblAddOutput)
+        Me.Name = "frmFunctionsPractice2"
+        Me.Text = "Functions Practice 2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblAddOutput As Label
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents txtAddNumber1 As TextBox
+    Friend WithEvents txtAddNumber2 As TextBox
+    Friend WithEvents btnResetAdd As Button
+    Friend WithEvents btnResetSubtract As Button
+    Friend WithEvents txtSubtractNumber2 As TextBox
+    Friend WithEvents txtSubtractNumber1 As TextBox
+    Friend WithEvents btnSubtract As Button
+    Friend WithEvents lblDifferenceOutput As Label
+    Friend WithEvents btnResetMultiplication As Button
+    Friend WithEvents txtMultiplicationNumber2 As TextBox
+    Friend WithEvents txtMultiplicationNumber1 As TextBox
+    Friend WithEvents btnMulitply As Button
+    Friend WithEvents lblProductOutput As Label
+    Friend WithEvents btnResetDivision As Button
+    Friend WithEvents txtDivisionNumber2 As TextBox
+    Friend WithEvents txtDivisionNumber1 As TextBox
+    Friend WithEvents btnDivide As Button
+    Friend WithEvents lblQuotientOutput As Label
+End Class
---
+++ functionsPracticeNumber2/Form1.vb
@@ -0,0 +1,348 @@
+﻿Public Class frmFunctionsPractice2
+
+    ' Variables to hold addition function input
+    Dim userInputNumber1 As Double = 0.00
+
+    Dim userInputNumber2 As Double = 0.00
+
+    ' Variables to hold subtraction function input
+    Dim userInputForSubtraction1 As Double = 0.00
+
+    Dim userInputForSubtraction2 As Double = 0.00
+
+    ' Variabel to hold multiplication function input
+    Dim userInputForMultiplication1 As Double = 0.00
+
+    Dim userInputForMultiplication2 As Double = 0.00
+
+    ' Variable to hold division funtion input
+    Dim userInputForDivision1 As Double = 0.00
+
+    Dim userInputForDivision2 As Double = 0.00
+
+
+
+
+    ''' <summary>
+    ''' Event handler for from initialization
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtAddNumber1.Select()
+    End Sub
+
+
+
+    ''' <summary>
+    ''' Add button event handler
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+        Dim additionNum1 As Double = 0.00
+        Dim additionNum2 As Double = 0.00
+
+        If Double.TryParse(txtAddNumber1.Text, userInputNumber1) Then
+
+            If (userInputNumber1 < 0 Or userInputNumber1 > 1000) = False Then
+
+                If Double.TryParse(txtAddNumber2.Text, userInputNumber2) Then
+
+                    If (userInputNumber2 < 0 Or userInputNumber2 > 1000) = False Then
+
+
+                        lblAddOutput.Text = add(additionNum1, additionNum2)
+                    Else
+                        MessageBox.Show("Please ensure value entered is between 0 and 1000 inclusive")
+                    End If
+                Else
+                    MessageBox.Show("Please ensure value entered is a valid Double")
+                End If
+            Else
+                MessageBox.Show("Please ensure value entered is between 0 and 1000 inclusive")
+
+            End If
+        Else
+            MessageBox.Show("Please ensure value entered is a valid Double")
+        End If
+
+
+    End Sub
+
+
+
+    ''' <summary>
+    ''' Add function 
+    ''' </summary>
+    ''' <param name="number1"></param>
+    ''' <param name="number2"></param>
+    ''' <returns></returns>
+    Private Function add(number1 As Double, number2 As Double) As Double
+
+        Dim sumOfAdd As Double = 0.0
+
+        number1 = userInputNumber1
+
+        number2 = userInputNumber2
+
+        sumOfAdd = number1 + number2
+
+
+
+
+        Return sumOfAdd
+
+    End Function
+
+    ''' <summary>
+    ''' Event handler for reset button of addition function
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnResetAdd_Click(sender As Object, e As EventArgs) Handles btnResetAdd.Click
+        txtAddNumber1.Text = ""
+        txtAddNumber2.Text = ""
+
+        lblAddOutput.Text = ""
+
+        userInputNumber1 = 0.00
+        userInputNumber2 = 0.00
+
+        txtAddNumber1.Select()
+
+    End Sub
+
+
+
+
+    ''' <summary>
+    ''' subtract button Event handler 
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnSubtract_Click(sender As Object, e As EventArgs) Handles btnSubtract.Click
+
+        Dim subtractionNumber1 As Double = 0.00
+
+        Dim subtractionNumber2 As Double = 0.00
+
+        If Double.TryParse(txtSubtractNumber1.Text, userInputForSubtraction1) Then
+
+            If (userInputForSubtraction1 < 0 Or userInputForSubtraction1 > 1000) = False Then
+
+                If Double.TryParse(txtSubtractNumber2.Text, userInputForSubtraction2) Then
+
+                    If (userInputForSubtraction2 < 0 Or userInputForSubtraction2 > 1000) = False Then
+
+                        lblDifferenceOutput.Text = subtract(subtractionNumber1, subtractionNumber2)
+
+                    Else
+                        MessageBox.Show("Please ensure value entered is between 0 and 1000 inclusive")
+                    End If
+                Else
+                    MessageBox.Show("Please ensure value entered is a valid Double")
+                End If
+
+            Else
+                MessageBox.Show("Please ensure value entered is between 0 and 1000 inclusive")
+            End If
+        Else
+            MessageBox.Show("Please ensure value entered is a valid Double")
+        End If
+
+    End Sub
+
+
+    ''' <summary>
+    ''' Subtract function
+    ''' </summary>
+    ''' <param name="sNum1"></param>
+    ''' <param name="sNum2"></param>
+    ''' <returns></returns>
+    Private Function subtract(sNum1 As Double, sNum2 As Double) As Double
+        Dim differenceOfSubtraction As Double = 0.00
+
+        sNum1 = userInputForSubtraction1
+
+        sNum2 = userInputForSubtraction2
+
+        differenceOfSubtraction = userInputForSubtraction1 - userInputForSubtraction2
+
+        Return differenceOfSubtraction
+    End Function
+
+    ''' <summary>
+    ''' Event handler for reset button of subtraction function
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnResetSubtract_Click(sender As Object, e As EventArgs) Handles btnResetSubtract.Click
+        txtSubtractNumber1.Text = ""
+        txtSubtractNumber2.Text = ""
+
+        lblDifferenceOutput.Text = ""
+
+        userInputForSubtraction1 = 0.00
+        userInputForSubtraction2 = 0.00
+
+        txtSubtractNumber1.Select()
+    End Sub
+
+
+
+    ''' <summary>
+    ''' multiply button event handler
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnMulitply_Click(sender As Object, e As EventArgs) Handles btnMulitply.Click
+        Dim multiplicationNum1 As Double = 0.00
+        Dim multiplicationNum2 As Double = 0.00
+
+        If Double.TryParse(txtMultiplicationNumber1.Text, userInputForMultiplication1) Then
+
+            If (userInputForMultiplication1 < 0 Or userInputForMultiplication1 > 1000) = False Then
+
+                If Double.TryParse(txtMultiplicationNumber2.Text, userInputForMultiplication2) Then
+
+                    If (userInputForMultiplication1 < 0 Or userInputForMultiplication2 > 1000) = False Then
+
+                        lblProductOutput.Text = multiply(multiplicationNum1, multiplicationNum2)
+
+                    Else
+                        MessageBox.Show("Please ensure value entered is between 0 and 1000 inclusive")
+                    End If
+                Else
+                    MessageBox.Show("Please ensure value entered is a valid Double")
+                End If
+
+
+            Else
+                MessageBox.Show("Please ensure value entered is between 0 and 1000 inclusive")
+            End If
+
+        Else
+            MessageBox.Show("Please ensure value entered is a valid Double")
+        End If
+
+    End Sub
+
+
+    ''' <summary>
+    ''' Multiply function
+    ''' </summary>
+    ''' <param name="mNum1"></param>
+    ''' <param name="mNum2"></param>
+    ''' <returns></returns>
+
+    Private Function multiply(mNum1 As Double, mNum2 As Double) As Double
+        Dim productOfMultiplication As Double = 0.00
+
+        mNum1 = userInputForMultiplication1
+
+        mNum2 = userInputForMultiplication2
+
+        productOfMultiplication = mNum1 * mNum2
+
+
+        Return productOfMultiplication
+    End Function
+
+
+    ''' <summary>
+    ''' Event handler for Reset multiplation button
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnResetMultiplication_Click(sender As Object, e As EventArgs) Handles btnResetMultiplication.Click
+        txtMultiplicationNumber1.Text = ""
+        txtMultiplicationNumber2.Text = ""
+
+        lblProductOutput.Text = ""
+
+        userInputForMultiplication1 = 0.00
+        userInputForMultiplication2 = 0.00
+
+        txtMultiplicationNumber1.Select()
+
+    End Sub
+
+
+
+
+
+
+    ''' <summary>
+    ''' Divide button event handler
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnDivide_Click(sender As Object, e As EventArgs) Handles btnDivide.Click
+
+        Dim divisionNum1 As Double = 0.00
+        Dim divisionNum2 As Double = 0.00
+
+        If Double.TryParse(txtDivisionNumber1.Text, userInputForDivision1) Then
+
+            If (userInputForDivision1 < 0 Or userInputForDivision1 > 1000) = False Then
+
+                If Double.TryParse(txtDivisionNumber2.Text, userInputForDivision2) Then
+
+                    If (userInputForDivision2 < 0 Or userInputForDivision2 > 1000) = False Then
+
+                        lblQuotientOutput.Text = divide(divisionNum1, divisionNum2)
+
+                    Else
+                        MessageBox.Show("Please ensure value entered is between 0 and 1000 inclusive")
+                    End If
+                Else
+                    MessageBox.Show("Please ensure value entered is a valid Double")
+                End If
+
+
+            Else
+                MessageBox.Show("Please ensure value entered is between 0 and 1000 inclusive")
+            End If
+
+        Else
+            MessageBox.Show("Please ensure value entered is a valid Double")
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Divide function
+    ''' </summary>
+    ''' <param name="dNum1"></param>
+    ''' <param name="dNum2"></param>
+    ''' <returns></returns>
+    Private Function divide(dNum1 As Double, dNum2 As Double) As Double
+        Dim quotientOfDivision As Double = 0.00
+
+        dNum1 = userInputForDivision1
+        dNum2 = userInputForDivision2
+
+        quotientOfDivision = dNum1 / dNum2
+
+        Return quotientOfDivision
+    End Function
+
+
+    ''' <summary>
+    ''' Event handler for reset divsion button
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnResetDivision_Click(sender As Object, e As EventArgs) Handles btnResetDivision.Click
+        txtDivisionNumber1.Text = ""
+        txtDivisionNumber2.Text = ""
+
+        lblQuotientOutput.Text = ""
+
+        userInputForDivision1 = 0.00
+        userInputForDivision2 = 0.00
+
+        txtDivisionNumber1.Select()
+    End Sub
+End Class
